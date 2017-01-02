7281fc759fcf633faa0e06fee381523c8ec3bd6c
opacitySun <809340810@qq.com>
2017-01-02 11:43:38 +0800

    版本1.0

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
 // nodejs 中的path模块
 var path = require('path');
 var stylishReporter = require('jshint-loader-reporter')('stylish');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
 var CleanWebpackPlugin = require('clean-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     // 入口文件，path.resolve()方法，可以结合我们给定的两个参数最后生成绝对路径，最终指向的就是我们的index.js文件
-    entry: path.resolve(__dirname, '../js/modules/index.js'),
+    entry: path.resolve(__dirname, 'js/modules/index.js'),
     // 输出配置
     output: {
         // 输出路径是 myProject/webapp
-        path: path.resolve(__dirname, '../webapp'),
+        path: path.resolve(__dirname, 'webapp'),
         publicPath: '/',
         filename: '[name].[hash].js',
         chunkFilename: '[id].[chunkhash].js'
@@ -19,10 +19,10 @@ module.exports = {
         extensions: ['', '.js', '.vue']
     },
     plugins: [
-        new CleanWebpackPlugin(['../webapp']),
+        new CleanWebpackPlugin(['webapp']),
         new HtmlWebpackPlugin({
             filename: 'index.html',
-            template: path.resolve(__dirname, '../views/index.html'),
+            template: path.resolve(__dirname, 'views/index.html'),
             inject: true,
             hash: false,
             cache: true,
