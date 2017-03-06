e823e62295bbc51488fa64bb129fbc64ff6743e4
opacitySun <809340810@qq.com>
2017-03-06 15:34:06 +0800

    版本1.0

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import VueResource from 'vue-resource';
 import Routes from './routes';
+import App from './app.vue';
 
 Vue.use(VueRouter);
 Vue.use(VueResource);
@@ -16,4 +17,9 @@ Vue.config.debug = true;
 
 const router = new VueRouter(Routes);
 
-new Vue({router: router}).$mount('#app');
+//new Vue({router: router}).$mount('#app');
+new Vue({
+    el: '#app',
+    router: router,
+    render: h => h(App)
+})
