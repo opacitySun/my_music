4bec946d103b5d6ddc132c901ff88c96548f967f
opacitySun <809340810@qq.com>
2017-03-07 15:13:15 +0800

    版本1.0

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,15 +1,15 @@
 <template>
     <div>
-    	<div class="weui_cells_title">推荐歌单</div>
+    	<div class="weui-cells__title">推荐歌单</div>
 		<div class="weui_cells weui_cells_access">
 			<router-link :to="item.href" v-for="(item,index) in musics" class="weui_cell">
-				<div class="weui_cell_hd">
+				<div class="weui-cell__hd">
 		            <img src="" alt="icon" style="width:20px;margin-right:5px;display:block">
 		        </div>
-		        <div class="weui_cell_bd weui_cell_primary">
+		        <div class="weui-cell__bd weui_cell_primary">
 		            <p>{{item.name}}</p>
 		        </div>
-		        <div class="weui_cell_ft">
+		        <div class="weui-cell__ft">
 		            {{item.desc}}
 		        </div>
 			</router-link>
