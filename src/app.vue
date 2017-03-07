bcbfa88bff421d29d28dfef853f418f6da49c204
opacitySun <809340810@qq.com>
2017-03-07 10:01:16 +0800

    版本1.0

--- src/app.vue
+++ src/app.vue
@@ -10,16 +10,16 @@
             <router-view></router-view>
         </main>
         <footer>
-        	<div class="col-sm-4 col-md-4 active">
-        		<i class="fa fa-music"></i>
+        	<div class="col-xs-4 col-sm-4 col-md-4 active">
+        		<i class="fa fa-3x fa-music"></i>
         		<p>发现音乐</p>
         	</div>
-        	<div class="col-sm-4 col-md-4">
-        		<i class="fa fa-music"></i>
+        	<div class="col-xs-4 col-sm-4 col-md-4">
+        		<i class="fa fa-3x fa-music"></i>
         		<p>我的音乐</p>
         	</div>
-        	<div class="col-sm-4 col-md-4">
-        		<i class="fa fa-user"></i>
+        	<div class="col-xs-4 col-sm-4 col-md-4">
+        		<i class="fa fa-3x fa-user"></i>
         		<p>帐号</p>
         	</div>
         </footer>
