527c2709d9627b0b771b3899baa802bceac52ca8
孙博为 <bowei.sun@acorn-net.com>
2017-03-28 14:43:49 +0800

    版本1.0

--- src/vuex/actions.js
+++ src/vuex/actions.js
@@ -1,6 +1,6 @@
 import * as types from './mutations';
 
-var const addToCart = ({ commit }, product) => {
+var addToCart = ({ commit }, product) => {
   if (product.inventory > 0) {
     commit(types.ADD_TO_CART, {
       id: product.id
