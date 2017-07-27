528a10505c2398f13354236e5eeac1a39a783bc6
opacitySun <809340810@qq.com>
2017-07-27 23:27:49 +0800

    版本1.0

--- src/js/index.js
+++ src/js/index.js
@@ -8,10 +8,11 @@ module.exports = {
 		var jsonStr = {};
 		jsonStr = JSON.stringify(jsonStr);
 		this.$http.jsonp(ResourcePath+'/getMusicList').then(function(res){
-			$.each(res.body.result,function(key,obj){
-				obj["href"] = "/music-detail/"+obj.id;
-				this.$data.musics.push(obj);
-			});
+			var res = res.body.result;
+			for(var k in res){
+				res[k]["href"] = "/music-detail/"+res[k].id;
+				this.musics.push(res[k]);
+			}
 		},function(err){
 			console.log(err);
 		});
