ab3690696a4163537747a0de908bab1c0c1bd5eb
opacitySun <809340810@qq.com>
2017-07-27 21:26:08 +0800

    版本1.0

--- src/js/index.js
+++ src/js/index.js
@@ -5,18 +5,30 @@ module.exports = {
 		}
 	},
 	created(){
-		this.$http({
-			method:'POST',
-            url:ResourcePath+'/getMusicList',
-            data:{},
-            headers: {"Content-Type":"application/json;charset=UTF-8"}
-		}).then(function(res){
-			$.each(res,function(key,obj){
-				obj["href"] = "/music-detail/"+obj.id;
-				this.musics.push(obj);
-			});
-		},function(err){
-			console.log(err);
+		$.ajax({
+			type:'post',
+			url:ResourcePath+'/getMusicList',
+			data:{},
+			dataType:'json',
+			success:function(res){
+				$.each(res,function(key,obj){
+					obj["href"] = "/music-detail/"+obj.id;
+					this.musics.push(obj);
+				});
+			}
 		});
+		// this.$http({
+		// 	method:'POST',
+  //           url:ResourcePath+'/getMusicList',
+  //           data:{},
+  //           headers: {"Content-Type":"application/json;charset=UTF-8"}
+		// }).then(function(res){
+		// 	$.each(res,function(key,obj){
+		// 		obj["href"] = "/music-detail/"+obj.id;
+		// 		this.musics.push(obj);
+		// 	});
+		// },function(err){
+		// 	console.log(err);
+		// });
 	}
 };
