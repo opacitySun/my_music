6530b83d512dc89ff59a45846325edb59f3240d2
孙博为 <bowei.sun@acorn-net.com>
2017-07-31 16:24:45 +0800

    版本1.0

--- src/views/reg.vue
+++ src/views/reg.vue
@@ -30,11 +30,11 @@
 					<img class="weui-vcode-img" src="">
 				</div>
 			</div>
-			<div class="weui-cells__tips"></div>
-		    <div class="weui-btn-area">
-		        <a href="javascript:void(0)" class="weui-btn weui-btn_plain-primary">注册</a>
-		    </div>
 		</div>
+		<div class="weui-cells__tips"></div>
+	    <div class="weui-btn-area">
+	        <a href="javascript:void(0)" class="weui-btn weui-btn_plain-primary">注册</a>
+	    </div>
     </div>
 </template>
 
