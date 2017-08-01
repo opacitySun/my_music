a9dd5be126adee67b1757efd633b158645684d6e
孙博为 <bowei.sun@acorn-net.com>
2017-08-01 14:55:39 +0800

    版本1.0

--- src/views/reg.vue
+++ src/views/reg.vue
@@ -22,7 +22,7 @@
 					<input class="weui-input" type="password" placeholder="请输入密码">
 				</div>
 			</div>
-			<div class="weui-cell weui-cell_vcode">
+			<div class="weui-cell">
 				<div class="weui-cell__hd"><label class="weui-label">验证码</label></div>
 				<div class="weui-cell__bd">
 					<input class="weui-input" type="number" placeholder="请输入验证码">
