cb200d4a3a40bf1980b79c8d801884c4a4f405d3
孙博为 <bowei.sun@acorn-net.com>
2017-08-01 16:10:22 +0800

    版本1.0

--- src/js/login.js
+++ src/js/login.js
@@ -5,12 +5,21 @@ module.exports = {
 		}
 	},
 	created(){
-		
+		this.getCodeFn();
 	},
 	methods:{
 		//注册
 		goRegister:function(){
 			this.$router.push({ path: '/reg' });
+		},
+		//获取验证码
+		getCodeFn:function(){
+			this.$http.jsonp(ResourcePath+'/getVerificationCodeAction').then(function(res){
+				res = res.body.result;
+				
+			},function(err){
+				console.log(err);
+			});
 		}
 	}
 };
