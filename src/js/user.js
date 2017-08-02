1972866c1b77dbb00f2dbf9045a9dfd453e68488
孙博为 <bowei.sun@acorn-net.com>
2017-08-02 13:26:07 +0800

    版本1.0

--- src/js/user.js
+++ src/js/user.js
@@ -11,25 +11,7 @@ module.exports = {
 		}
 	},
 	created(){
-		var userUuid = window.localStorage.getItem("userUuid");
-		if(userUuid){
-			this.$http.jsonp(ResourcePath+'/getUserInfoAction?uuid='+userUuid).then(function(res){
-				res = res.body.result;
-				this.user.name = res.name;
-				this.user.img = ResourcePath + res.img;
-				this.user.points = res.points;
-				this.loginBtnClass = "hidden";
-				this.outloginBtnClass = "";
-			},function(err){
-				console.log(err);
-			});
-		}else{
-			this.user.name = '未知';
-			this.user.img = ResourcePath + "/files/default.png";
-			this.user.points = 0;
-			this.loginBtnClass = "";
-			this.outloginBtnClass = "hidden";
-		}
+		this.getPageInfoAsUuid();
 	},
 	methods:{
 		//登录
@@ -39,11 +21,33 @@ module.exports = {
 		//退出登录
 		outLogin:function(){
 			window.localStorage.removeItem("userUuid");
-			this.$router.push({ path: '/',redirect:'/user' });
+			this.getPageInfoAsUuid();
 		},
 		//注册
 		goRegister:function(){
 			this.$router.push({ path: '/reg' });
+		},
+		//判断是否存在用户uuid
+		getPageInfoAsUuid:function(){
+			var userUuid = window.localStorage.getItem("userUuid");
+			if(userUuid){
+				this.$http.jsonp(ResourcePath+'/getUserInfoAction?uuid='+userUuid).then(function(res){
+					res = res.body.result;
+					this.user.name = res.name;
+					this.user.img = ResourcePath + res.img;
+					this.user.points = res.points;
+					this.loginBtnClass = "hidden";
+					this.outloginBtnClass = "";
+				},function(err){
+					console.log(err);
+				});
+			}else{
+				this.user.name = '未知';
+				this.user.img = ResourcePath + "/files/default.png";
+				this.user.points = 0;
+				this.loginBtnClass = "";
+				this.outloginBtnClass = "hidden";
+			}
 		}
 	}
 };
