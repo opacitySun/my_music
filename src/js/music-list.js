ef6eb151daae5889d0bea84b45bbc598992c4872
孙博为 <bowei.sun@acorn-net.com>
2017-08-02 16:08:45 +0800

    版本1.0

--- src/js/music-list.js
+++ src/js/music-list.js
@@ -7,15 +7,25 @@ module.exports = {
 		};
 	},
 	created(){
-		var userId = this.$route.params.id;
-		this.$http.jsonp(ResourcePath+'/getMyMusicList').then(function(res){
-			res = res.body.result;
-			for(var k in res){
-				res[k]["href"] = "/music-detail/"+res[k].id;
-				this.musics.push(res[k]);
+		
+	},
+	methods:{
+		//根据uuid获取列表数据
+		getListAsUuid:function(){
+			var userUuid = window.localStorage.getItem("userUuid");
+			if(userUuid){
+				this.$http.jsonp(ResourcePath+'/getUserMusicList?uuid='+userUuid).then(function(res){
+					res = res.body.result;
+					for(var k in res){
+						res[k]["href"] = "/music-detail/"+res[k].id;
+						this.myMusics.push(res[k]);
+					}
+				},function(err){
+					console.log(err);
+				});
+			}else{
+				this.$router.push({ path: '/login' });
 			}
-		},function(err){
-			console.log(err);
-		});
+		}
 	}
 };
--- src/views/music-list.vue
+++ src/views/music-list.vue
@@ -2,7 +2,7 @@
     <div>
 		<div class="weui-cells__title" style="padding-top: .77em;margin-top:0;">我的歌单</div>
         <div class="weui-cells">
-        	<router-link :to="item.href" v-for="(item,index) in musics" class="weui-cell weui-cell_access">
+        	<router-link :to="item.href" v-for="(item,index) in myMusics" class="weui-cell weui-cell_access">
 				<div class="weui-cell__hd">
                 	<img :src="item.img" style="width:2.4rem;margin-right:1rem;display:block"></div>
                 <div class="weui-cell__bd">
